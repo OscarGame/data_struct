cda6f20ed94ab6c4771d92310c117c5e4dcd5b9b
carl <zhongzheng.wang1986@gmail.com>
2015-01-23 15:56:01 +0800

    增加二叉树和二叉搜索树的简单演示伪代码

--- README.md
+++ README.md
@@ -5,17 +5,22 @@ data_struct
 在网上会经常发现一些数据结构的很神奇的实现，自己学习工作过程中为了练手也会在此留下痕迹，
 
 ```
+├── binary_search.c
+├── bin_tree.c
 ├── list
 │   ├── list.c
 │   ├── list.h
 │   ├── makefile
-│   └── ut\_list.c
+│   └── ut_list.c
+├── max_sub_seq_sum.c
 ├── README.md
-└── sort
-    └── sort.c
+└── sort.c
 ```
 
 - [x] [linux循环链表学习](http://blog.csdn.net/wzzfeitian/article/details/7816614)，这是之前在csdn写的博客，记录了该实现的一些想法 ./list/
 - [ ] redis里面各种数据结构的实现
 - [x] 几种排序算法的实现（插入排序，选择排序，冒泡排序，希尔排序，快速排序） ./sort.c
 - [x] 最大子列和问题，./max_sub_seq_sum.c中使用了在线处理算法，时间复杂度为O(N)
+- [x] 增加二叉树的伪代码 ./bin_tree.c，简单描述了二叉树的常用操作
+- [x] 二叉搜索树的简单操作 ./bin_tree.c
+- [x] 增加二分查找的演示伪代码 ./binary_search.c
---
+++ bin_tree.c
@@ -0,0 +1,270 @@
+
+
+typedef struct _tree_node
+{
+    element_type m_data;
+    struct _tree_node *left;
+    struct _tree_node *right;
+} tree_node;
+
+
+// 递归实现先序遍历, root, left, right
+void preorder_traversal(tree_node *bt)
+{
+    if (bt)
+    {
+        printf("%d", bt->data);
+        preorder_traversal(bt->left);
+        preorder_traversal(bt->right);
+    }
+}
+
+// 递归实现中序遍历, left, root, right
+void inorder_traversal(tree_node *bt)
+{
+    if (bt)
+    {
+        inorder_traversal(bt->left);
+        printf("%d", bt->data);
+        inorder_traversal(bt->right);
+    }
+}
+
+// 递归实现后序遍历, left, right, root
+void postorder_traversal(tree_node *bt)
+{
+    if (bt)
+    {
+        postorder_traversal(bt->left);
+        postorder_traversal(bt->right);
+        printf("%d", bt->data);
+    }
+}
+
+
+// 非递归实现中序遍历，使用堆栈
+// 1. 遇到一个结点，就把它压栈，并去遍历它的左子树；
+// 2. 当左子树遍历结束后，从栈顶弹出这个结点，并访问它
+// 3. 然后按其右子树指针(当作root结点)再去中序遍历该节点的右子树
+void inorder_traversal_norecurse(tree_node *bt)
+{
+    tree_node *t = bt;
+    stack s = create_stack(MAXSIZE); // 创建并初始化堆栈
+    while (t || !is_empty(s))
+    {
+        while (t) // 一直向左，并将沿途结点压栈
+        {
+            push(s, t); // 第一次碰到该结点
+            t = t->left;
+        }
+
+        if (!is_empty(s))
+        {
+            t = pop(s); // 结点出栈，第二次碰到该几点
+            printf("%d", t->data); // 访问出栈结点
+            t = t->right; // 转向右子树
+        }
+    }
+}
+
+void preorder_traversal_norecurse(tree_node *bt)
+{
+    tree_node *t = bt;
+    stack s = create_stack(MAXSIZE); // 创建并初始化堆栈
+    while (t || !is_empty(s))
+    {
+        while (t) // 一直向左，并将沿途结点压栈
+        {
+            push(s, t); // 第一次碰到该结点
+            printf("%d", t->data); // 访问出栈结点
+            t = t->left;
+        }
+
+        if (!is_empty(s))
+        {
+            t = pop(s); // 结点出栈，第二次碰到该几点
+            t = t->right; // 转向右子树
+        }
+    }
+}
+
+// 层序遍历
+// 1. 先根结点入队
+// 2. 从队列中取出一个结点，访问
+// 3. 若该结点的左右子树非空，将左右子树指针顺序入队
+void levelorder_traversal(tree_node *bt)
+{
+    queue q;
+    tree_node *t = NULL;
+    if (!bt) // 若是空树，直接返回
+    {
+        return;
+    }
+
+    q = create_queue(MAXSIZE); // 创建并初始化队列q
+    add_q(q, bt);
+    while (!is_empty(q))
+    {
+        t = delete_q(q);
+        printf("%d", t->data); // 访问取出队列的结点
+        if (t->left)
+        {
+            add_q(q, t->left);
+        }
+        if (t->right)
+        {
+            add_q(q, t->right);
+        }
+    }
+}
+
+// 二叉搜索树(二叉查找树, 二叉排序树)
+// left < root < right
+
+position find(element_type x, tree_node *bst)
+{
+    if (!bst)
+    {
+        return NULL;
+    }
+
+    if (x > bst->data)
+    {
+        return find(x, bst->right);
+    }
+    else if (x < bst->data)
+    {
+        return find(x, bst->left);
+    }
+    else
+    {
+        return bst;
+    }
+}
+
+// 非递归实现
+position iter_find(element_type x, tree_node *bst)
+{
+    if (!bst)
+    {
+        return NULL;
+    }
+
+    while (bst)
+    {
+        if (x > bst->data)
+        {
+            bst = bst->right;
+        }
+        else if (x < bst->data)
+        {
+            bst = bst->left;
+        }
+        else
+        {
+            return bst;
+        }
+    }
+
+    return NULL;
+}
+
+// find_min用递归实现
+position find_min(tree_node *bst)
+{
+    if (!bst)
+    {
+        return NULL;
+    }
+
+    if (bst->left)
+    {
+        return find_min(bst->left);
+    }
+
+    return bst;
+}
+
+// find_max用loop实现
+position find_max(tree_node *bst)
+{
+    if (!bst)
+    {
+        return NULL;
+    }
+
+    while (bst->right)
+    {
+        bst = bst->right;
+    }
+
+    return bst;
+}
+
+tree_node *insert(element_type x, tree_node *bst)
+{
+    if (!bst)
+    {
+        bst = malloc(sizeof(struct tree_node));
+        bst->data = x;
+        bst->left = bst->right = NULL;
+    }
+    else
+    {
+        if (x < bst->data)
+        {
+            bst->left = insert(x, bst->left);
+        }
+        else if (x > bst->data)
+        {
+            bst->right = insert(x, bst->right);
+        }
+        // else X已经存在，什么都不做
+    }
+
+    return bst;
+}
+
+tree_node *delete(element_type x, tree_node *bst)
+{
+    position tmp;
+    if (!bst)
+    {
+        printf("要删除的元素未找到");
+    }
+    else if (x < bst->data)
+    {
+        bst->left = delete(x, bst->left);
+    }
+    else if (x > bst->data)
+    {
+        bst->right = delete(x, bst->right);
+    }
+    else // 找到要删除的结点
+    {
+        if (bst->left && bst->right)
+        {
+            tmp = find_min(bst->right); // 找到右子树的最小结点（该结点肯定只有0或1个子结点）
+            bst->data = tmp->data;
+            bst->right = delete(bst->data, bst->right); // 删除右子树的最小结点
+        }
+        else // 被删除结点只有0或1个子结点
+        {
+            tmp = bst;
+            if (!bst->left)
+            {
+                bst = bst->right;
+            }
+            else if (!bst->right)
+            {
+                bst = bst->left;
+            }
+
+            free(tmp);
+        }
+    }
+
+    return bst;
+}
+
+
